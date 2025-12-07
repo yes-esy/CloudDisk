2d886274fc5bf2021663e8871ae34f5b4d04ac47
Sheng <2900226123@qq.com>
2025-12-07 20:31:45 +0800

    Init project

---
+++ build/CMakeFiles/CloudDisk.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CloudDisk.dir/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.c.o"
+  "CMakeFiles/CloudDisk.dir/CMakeFiles/3.28.3/CompilerIdC/CMakeCCompilerId.c.o.d"
+  "CMakeFiles/CloudDisk.dir/src/main.c.o"
+  "CMakeFiles/CloudDisk.dir/src/main.c.o.d"
+  "CloudDisk"
+  "CloudDisk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CloudDisk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
