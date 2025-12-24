208c4489280216b8ab7ebf67abe8a3ac50098288
Shane <2184275713@qq.com>
2025-12-24 17:58:01 +0800

    virtual_fs table create

--- src/sql/table.sql
+++ src/sql/table.sql
@@ -5,3 +5,20 @@ CREATE TABLE t_user (
     cryptpasswd VARCHAR(255) NOT NULL COMMENT '加密密码',
     pwd VARCHAR(255) COMMENT '当前工作目录'
 )COMMENT='用户表';
+
+-- 假设最多 5 层目录
+WITH RECURSIVE path_tree AS (
+  SELECT id, parent_id, filename, type, 0 AS depth
+  FROM virtual_fs
+  WHERE parent_id = 0 AND owner_id = 1001 AND filename = 'a'
+
+  UNION ALL
+
+  SELECT v.id, v.parent_id, v.filename, v.type, pt.depth + 1
+  FROM virtual_fs v
+  JOIN path_tree pt ON v.parent_id = pt.id
+  WHERE v.owner_id = 1001
+)
+SELECT id, md5, filesize
+FROM path_tree
+WHERE filename = 'report.pdf' AND type = 1;
